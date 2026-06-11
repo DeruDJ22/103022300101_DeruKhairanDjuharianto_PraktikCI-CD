0cf6606d74297da2e58dae2209dda13265140897
KuroAkai <derudj12345@gmail.com>
2026-06-11 09:01:20 +0700

    fix: Fix tes gagal

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -6,21 +6,21 @@ public class CounterTest {
 	@Test
 	public void testReset() {
 		Counter testCounter = new Counter();
-		
-		for(int i = 0; i <10; i++) {
+
+		for (int i = 0; i < 10; i++) {
 			testCounter.increment();
 		}
-		
+
 		testCounter.reset();
-		
+
 		assertEquals(testCounter.getCount(), 0);
 	}
 
 	@Test
 	public void testIncrement() {
 		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
+
+		for (int i = 1; i < 10; i++) {
 			testCounter.increment();
 			assertEquals(testCounter.getCount(), i);
 		}
@@ -29,16 +29,10 @@ public class CounterTest {
 	@Test
 	public void testDecrement() {
 		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
+
+		for (int i = 1; i < 10; i++) {
 			testCounter.decrement();
 			assertEquals(testCounter.getCount(), i * -1);
 		}
-	}
-
-	@Test
-	public void testIntentionalFailure() {
-		// Tes ini sengaja dibuat gagal untuk mencoba fitur Continuous Testing di GitHub Actions
-		assertEquals("Tes ini sengaja disalahkan", 1, 2);
 	}
 }
